9345b3aab308203ff4d66ecf13821ad5c8cb48fc
Gabriel <gabrielpimentelr1@gmail.com>
2021-05-24 12:43:19 -0300

    Adicionei um trampolim

--- Assets/Scripts/Trampoline.cs
+++ Assets/Scripts/Trampoline.cs
@@ -5,22 +5,20 @@ using UnityEngine;
 public class Trampoline : MonoBehaviour
 {
     public float jumpForce;
-    // Start is called before the first frame update
+    private Animator anim;
+
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  
         void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.tag == "Player")
         {
+            anim.SetTrigger("jump");
             collision.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+
         }
     }
 }
