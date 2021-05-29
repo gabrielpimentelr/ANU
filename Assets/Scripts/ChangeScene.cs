73213076ff5f2549acd616951144f581d843c2ec
Gabriel <gabrielpimentelr1@gmail.com>
2021-05-28 23:43:35 -0300

    Mudança de cena lvl 1 pra 2

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -15,6 +15,13 @@ public void Sair()
 {
     Application.Quit();
 }
+void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(nomeDaCena);
+        }
 
 
+    }
 }
