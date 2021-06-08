2c40d50e7ae3f448156f05f0d6c58ae18c38200f
Paulo Henrique <juniorhenrique.1@hotmail.com>
2021-06-08 14:30:29 -0300

    database

---
+++ Assets/Scripts/User.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class User 
+{
+  public string UserName;
+  public string Tempo;
+  public string Morte;
+}
